6971b466bb9e9c3ce568be4f4c408dbb7934e9c7
Vidal-Gustavo <vidal47pro@gmail.com>
2025-10-06 15:57:48 -0300

    Implementacao completa dos celulares

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia(numero: "111111111", modelo: "Nokia v1", imei: "7165asdsd", memoria: 8);
+Console.WriteLine("Teste Nokia");
+n1.Ligar();
+n1.InstalarAplicativo("Whatsapp");
+n1.ReceberLigacao();
+
+Console.WriteLine("----------------");
+
+Iphone ip1 = new Iphone(numero: "222222222", modelo: "Iphone v1", imei: "2763askg", memoria: 16);
+Console.WriteLine("Teste Iphone");
+ip1.Ligar();
+ip1.InstalarAplicativo("Instagram");
+ip1.ReceberLigacao();
+
